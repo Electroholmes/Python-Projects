75b387f423757128a850c41fd07b02b28ddc4f9a
ElectroHolmes <69202211+Electroholmes@users.noreply.github.com>
2020-10-21 22:51:26 +0530

    Add files via upload

---
+++ Site Connectivity/Site connectivity.py
@@ -0,0 +1,6 @@
+import socket
+IPaddress=socket.gethostbyname(socket.gethostname())
+if IPaddress=="127.0.0.1":
+    print("No internet, your localhost is "+ IPaddress)
+else:
+    print("Connected, with the IP address: "+ IPaddress )
